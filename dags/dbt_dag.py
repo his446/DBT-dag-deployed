0309f203e0feaecf088e7fd010ec299fa3c9d52f
hisoka <hisoka>
2025-08-08 16:30:48 +0200

    fix the dag for the dbt models

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -2,8 +2,10 @@ import os
 from datetime import datetime
 
 
-from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig
+
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+from cosmos.constants import TestBehavior
 
 
 profile_config = ProfileConfig(
@@ -15,12 +17,21 @@ profile_config = ProfileConfig(
     )
 )
 
+project_config=ProjectConfig("/usr/local/airflow/dags/dbt/data_pipeline",)
+project_dir="/usr/local/airflow/dags/dbt/data_pipeline"
+execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",)
 dbt_snowflake_dag = DbtDag(
     project_config=ProjectConfig("/usr/local/airflow/dags/dbt/data_pipeline",),
-    operator_args={"install_deps": True},
+    operator_args={
+        "install_deps": True,
+        "defer_tests": True 
+        },
     profile_config=profile_config,
     execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
-    schedule_interval="@daily",
+    render_config=RenderConfig(
+        test_behavior=TestBehavior.AFTER_ALL
+        ),
+    schedule="30 12 * * *",
     start_date=datetime(2023, 9, 10),
     catchup=False,
     dag_id="dbt_dag",
